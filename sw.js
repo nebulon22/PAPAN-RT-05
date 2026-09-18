7e00758152f07ceeb2533a158fbe0f306d9c3e3a
nebulon22 <42180377+nebulon22@users.noreply.github.com>
2026-09-18 17:00:39 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('jimpitan-app-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './kentongan.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
