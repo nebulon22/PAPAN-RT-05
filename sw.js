8460e6611311234eaef122ade001eae0ce521078
nebulon22 <42180377+nebulon22@users.noreply.github.com>
2026-09-18 21:41:01 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,29 @@
+const CACHE_NAME = 'jimpitan-app-v2';
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Paksa Service Worker baru langsung aktif
   e.waitUntil(
-    caches.open('jimpitan-app-v1').then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         './',
         './index.html',
         './kentongan-slit-drum.png'
       ]);
+    })
+  );
+});
+
+// Otomatis hapus cache versi lama di HP
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
